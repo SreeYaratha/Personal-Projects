6ba712599ce153b2130dbb062b487e5c51c5eac9
SreeYaratha <124852499+SreeYaratha@users.noreply.github.com>
2023-07-16 15:38:35 -0400

    Add files via upload

---
+++ MinimumSnippet.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class MinimumSnippet {
+
+	
+	public MinimumSnippet(Iterable<String> document, List<String> terms) {
+	
+		throw new UnsupportedOperationException("YOU MUST IMPLEMENT THIS!");
+	}
+
+	public boolean foundAllTerms() {
+
+		throw new UnsupportedOperationException("YOU MUST IMPLEMENT THIS!");
+
+	}
+
+	public int getStartingPos() {
+	
+		throw new UnsupportedOperationException("YOU MUST IMPLEMENT THIS!");
+
+	}
+
+	public int getEndingPos() {
+		
+		throw new UnsupportedOperationException("YOU MUST IMPLEMENT THIS!");
+
+	}
+
+	public int getLength() {
+	
+		throw new UnsupportedOperationException("YOU MUST IMPLEMENT THIS!");
+
+	}
+
+	public int getPos(int index) {
+		
+		throw new UnsupportedOperationException("YOU MUST IMPLEMENT THIS!");
+
+	}
+
+}
